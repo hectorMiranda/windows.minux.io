a2b4163da32a82aae23e69de8df6251acc3ed0f2
Hector Miranda <marcetux@gmail.com>
2025-06-03 21:02:27 -0700

    draf for client

---
+++ main.cpp
@@ -0,0 +1,52 @@
+// minux-client: a minimal customizable widget for Windows
+
+#include <windows.h>
+
+const char CLASS_NAME[] = "MinuxClient";
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        SetBkMode(hdc, TRANSPARENT);
+        SetTextColor(hdc, RGB(255, 255, 255));
+        TextOut(hdc, 20, 20, "Minux Client Widget", 21);
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int) {
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW,
+        CLASS_NAME, "Minux Client",
+        WS_POPUP,
+        100, 100, 300, 100,
+        NULL, NULL, hInstance, NULL
+    );
+
+    SetLayeredWindowAttributes(hwnd, 0, 230, LWA_ALPHA); // Transparency 0-255
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
