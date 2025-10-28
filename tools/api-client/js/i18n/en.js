6e84b20e0f8a36850bde08507d43619670c85d1a
Hector Miranda <marcetux@gmail.com>
2025-10-28 13:14:26 -0700

    api-client: add English (en) locale

---
+++ tools/api-client/js/i18n/en.js
@@ -0,0 +1,13 @@
+// English locale
+export const en = {
+  "send": "Send",
+  "save": "Save",
+  "new_request": "New Request",
+  "params": "Params",
+  "headers": "Headers",
+  "body": "Body",
+  "auth": "Auth",
+  "ready": "Ready.",
+  "environments": "Environments",
+  "history": "History"
+};
