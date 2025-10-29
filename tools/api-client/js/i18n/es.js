f40ecefb01767ec1efb4935ba16493bf167d20f1
Hector Miranda <marcetux@gmail.com>
2025-10-29 11:31:49 -0700

    api-client: add Español (es) locale

---
+++ tools/api-client/js/i18n/es.js
@@ -0,0 +1,13 @@
+// Español locale
+export const es = {
+  "send": "Enviar",
+  "save": "Guardar",
+  "new_request": "Nueva petición",
+  "params": "Parámetros",
+  "headers": "Cabeceras",
+  "body": "Cuerpo",
+  "auth": "Autenticación",
+  "ready": "Listo.",
+  "environments": "Entornos",
+  "history": "Historial"
+};
