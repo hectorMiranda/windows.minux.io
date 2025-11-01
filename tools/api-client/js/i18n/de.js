bd8bbdff825682a4b575f09a023b1a0551c80246
Hector Miranda <marcetux@gmail.com>
2025-10-31 20:05:35 -0700

    api-client: add Deutsch (de) locale

---
+++ tools/api-client/js/i18n/de.js
@@ -0,0 +1,13 @@
+// Deutsch locale
+export const de = {
+  "send": "Senden",
+  "save": "Speichern",
+  "new_request": "Neue Anfrage",
+  "params": "Parameter",
+  "headers": "Kopfzeilen",
+  "body": "Inhalt",
+  "auth": "Authentifizierung",
+  "ready": "Bereit.",
+  "environments": "Umgebungen",
+  "history": "Verlauf"
+};
