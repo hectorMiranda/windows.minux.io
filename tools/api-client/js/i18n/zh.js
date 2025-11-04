6111865677aca32bc41bd2bdbda33af1a3923b89
Hector Miranda <marcetux@gmail.com>
2025-11-04 12:13:07 -0800

    api-client: add 中文 (zh) locale

---
+++ tools/api-client/js/i18n/zh.js
@@ -0,0 +1,13 @@
+// 中文 locale
+export const zh = {
+  "send": "发送",
+  "save": "保存",
+  "new_request": "新建请求",
+  "params": "参数",
+  "headers": "请求头",
+  "body": "正文",
+  "auth": "认证",
+  "ready": "就绪。",
+  "environments": "环境",
+  "history": "历史"
+};
