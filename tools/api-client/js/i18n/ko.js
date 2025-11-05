81f6542931c56d04c77278a2dea5d64489417650
Hector Miranda <marcetux@gmail.com>
2025-11-05 10:30:30 -0800

    api-client: add 한국어 (ko) locale

---
+++ tools/api-client/js/i18n/ko.js
@@ -0,0 +1,13 @@
+// 한국어 locale
+export const ko = {
+  "send": "보내기",
+  "save": "저장",
+  "new_request": "새 요청",
+  "params": "매개변수",
+  "headers": "헤더",
+  "body": "본문",
+  "auth": "인증",
+  "ready": "준비됨.",
+  "environments": "환경",
+  "history": "기록"
+};
