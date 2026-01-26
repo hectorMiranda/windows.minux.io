1fd9ed5ab0defdf91726da3fc7c50ba8402cd884
Hector Miranda <marcetux@gmail.com>
2026-01-26 15:44:56 -0800

    data: add 101 Switching Protocols to HTTP status reference

--- tools/api-client/js/data/http-status.js
+++ tools/api-client/js/data/http-status.js
@@ -1,3 +1,4 @@
 export const HTTP_STATUS = [
-  {"code": 100, "text": "Continue", "class": "informational"}
+  {"code": 100, "text": "Continue", "class": "informational"},
+  {"code": 101, "text": "Switching Protocols", "class": "informational"}
 ];
