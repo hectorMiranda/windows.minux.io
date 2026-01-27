0c7dd9f36d8c2c2eda8e092ea9d75deb92026635
Hector Miranda <marcetux@gmail.com>
2026-01-27 13:01:19 -0800

    data: add 102 Processing to HTTP status reference

--- tools/api-client/js/data/http-status.js
+++ tools/api-client/js/data/http-status.js
@@ -1,4 +1,5 @@
 export const HTTP_STATUS = [
   {"code": 100, "text": "Continue", "class": "informational"},
-  {"code": 101, "text": "Switching Protocols", "class": "informational"}
+  {"code": 101, "text": "Switching Protocols", "class": "informational"},
+  {"code": 102, "text": "Processing", "class": "informational"}
 ];
