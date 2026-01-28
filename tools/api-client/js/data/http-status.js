4ace99a2ddb80e2b4bdfb2d2c08329747a48910e
Hector Miranda <marcetux@gmail.com>
2026-01-28 11:18:42 -0800

    data: add 103 Early Hints to HTTP status reference

--- tools/api-client/js/data/http-status.js
+++ tools/api-client/js/data/http-status.js
@@ -1,5 +1,6 @@
 export const HTTP_STATUS = [
   {"code": 100, "text": "Continue", "class": "informational"},
   {"code": 101, "text": "Switching Protocols", "class": "informational"},
-  {"code": 102, "text": "Processing", "class": "informational"}
+  {"code": 102, "text": "Processing", "class": "informational"},
+  {"code": 103, "text": "Early Hints", "class": "informational"}
 ];
