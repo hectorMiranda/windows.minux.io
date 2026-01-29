45ca7f3d75b447a077c01c39cb9a0be6db7c8bc8
Hector Miranda <marcetux@gmail.com>
2026-01-29 09:35:05 -0800

    data: add 200 OK to HTTP status reference

--- tools/api-client/js/data/http-status.js
+++ tools/api-client/js/data/http-status.js
@@ -2,5 +2,6 @@ export const HTTP_STATUS = [
   {"code": 100, "text": "Continue", "class": "informational"},
   {"code": 101, "text": "Switching Protocols", "class": "informational"},
   {"code": 102, "text": "Processing", "class": "informational"},
-  {"code": 103, "text": "Early Hints", "class": "informational"}
+  {"code": 103, "text": "Early Hints", "class": "informational"},
+  {"code": 200, "text": "OK", "class": "success"}
 ];
