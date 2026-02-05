4575f2b6e41c92391d865f794b47c8cad09ddef0
Hector Miranda <marcetux@gmail.com>
2026-02-05 08:34:46 -0800

    data: add 207 Multi-Status to HTTP status reference

--- tools/api-client/js/data/http-status.js
+++ tools/api-client/js/data/http-status.js
@@ -9,5 +9,6 @@ export const HTTP_STATUS = [
   {"code": 203, "text": "Non-Authoritative Information", "class": "success"},
   {"code": 204, "text": "No Content", "class": "success"},
   {"code": 205, "text": "Reset Content", "class": "success"},
-  {"code": 206, "text": "Partial Content", "class": "success"}
+  {"code": 206, "text": "Partial Content", "class": "success"},
+  {"code": 207, "text": "Multi-Status", "class": "success"}
 ];
