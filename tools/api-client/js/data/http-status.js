3c62f9d262f84f16fbe5dfd765a512475e65b8f8
Hector Miranda <marcetux@gmail.com>
2026-03-05 17:30:30 -0800

    data: add 418 I'm a teapot to HTTP status reference

--- tools/api-client/js/data/http-status.js
+++ tools/api-client/js/data/http-status.js
@@ -37,5 +37,6 @@ export const HTTP_STATUS = [
   {"code": 414, "text": "URI Too Long", "class": "client-error"},
   {"code": 415, "text": "Unsupported Media Type", "class": "client-error"},
   {"code": 416, "text": "Range Not Satisfiable", "class": "client-error"},
-  {"code": 417, "text": "Expectation Failed", "class": "client-error"}
+  {"code": 417, "text": "Expectation Failed", "class": "client-error"},
+  {"code": 418, "text": "I'm a teapot", "class": "client-error"}
 ];
