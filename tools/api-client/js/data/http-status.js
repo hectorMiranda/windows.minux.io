ce75ad3c5bff7637b29c76591980d1c23954ea76
Hector Miranda <marcetux@gmail.com>
2026-03-13 14:46:34 -0700

    data: add 429 Too Many Requests to HTTP status reference

--- tools/api-client/js/data/http-status.js
+++ tools/api-client/js/data/http-status.js
@@ -45,5 +45,6 @@ export const HTTP_STATUS = [
   {"code": 424, "text": "Failed Dependency", "class": "client-error"},
   {"code": 425, "text": "Too Early", "class": "client-error"},
   {"code": 426, "text": "Upgrade Required", "class": "client-error"},
-  {"code": 428, "text": "Precondition Required", "class": "client-error"}
+  {"code": 428, "text": "Precondition Required", "class": "client-error"},
+  {"code": 429, "text": "Too Many Requests", "class": "client-error"}
 ];
