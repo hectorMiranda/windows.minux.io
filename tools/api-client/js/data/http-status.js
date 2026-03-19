880930ab784f6383c16cdc3588d848a10abf324e
Hector Miranda <marcetux@gmail.com>
2026-03-18 17:11:29 -0700

    data: add 502 Bad Gateway to HTTP status reference

--- tools/api-client/js/data/http-status.js
+++ tools/api-client/js/data/http-status.js
@@ -50,5 +50,6 @@ export const HTTP_STATUS = [
   {"code": 431, "text": "Request Header Fields Too Large", "class": "client-error"},
   {"code": 451, "text": "Unavailable For Legal Reasons", "class": "client-error"},
   {"code": 500, "text": "Internal Server Error", "class": "server-error"},
-  {"code": 501, "text": "Not Implemented", "class": "server-error"}
+  {"code": 501, "text": "Not Implemented", "class": "server-error"},
+  {"code": 502, "text": "Bad Gateway", "class": "server-error"}
 ];
