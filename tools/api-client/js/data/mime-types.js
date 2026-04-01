c22f66c754aef924db9942b97e51c3c74b3ef241
Hector Miranda <marcetux@gmail.com>
2026-03-31 17:52:28 -0700

    data: map .txt to text/plain in MIME reference

--- tools/api-client/js/data/mime-types.js
+++ tools/api-client/js/data/mime-types.js
@@ -2,5 +2,6 @@ export const MIME_TYPES = [
   {"ext": "json", "type": "application/json"},
   {"ext": "xml", "type": "application/xml"},
   {"ext": "html", "type": "text/html"},
-  {"ext": "htm", "type": "text/html"}
+  {"ext": "htm", "type": "text/html"},
+  {"ext": "txt", "type": "text/plain"}
 ];
