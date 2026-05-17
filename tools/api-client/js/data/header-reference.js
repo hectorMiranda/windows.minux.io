2aff39d357ed289e9d78a707e82a94a3eee96851
Hector Miranda <marcetux@gmail.com>
2026-05-17 14:11:29 -0700

    data: document 'Accept-Encoding' header in reference

--- tools/api-client/js/data/header-reference.js
+++ tools/api-client/js/data/header-reference.js
@@ -1,3 +1,4 @@
 export const HEADER_REFERENCE = [
-  {"name": "Accept", "description": "Media types acceptable for the response."}
+  {"name": "Accept", "description": "Media types acceptable for the response."},
+  {"name": "Accept-Encoding", "description": "Acceptable content encodings (gzip, br)."}
 ];
