d02ae607fabedcbd011692a30d734f198a39e20f
Hector Miranda <marcetux@gmail.com>
2026-05-19 10:45:15 -0700

    data: document 'Authorization' header in reference

--- tools/api-client/js/data/header-reference.js
+++ tools/api-client/js/data/header-reference.js
@@ -1,5 +1,6 @@
 export const HEADER_REFERENCE = [
   {"name": "Accept", "description": "Media types acceptable for the response."},
   {"name": "Accept-Encoding", "description": "Acceptable content encodings (gzip, br)."},
-  {"name": "Accept-Language", "description": "Preferred natural languages."}
+  {"name": "Accept-Language", "description": "Preferred natural languages."},
+  {"name": "Authorization", "description": "Credentials for authenticating the request."}
 ];
