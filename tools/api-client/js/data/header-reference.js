43ae5c0e46c879b11d1d65864f6e7aa08536b01e
Hector Miranda <marcetux@gmail.com>
2026-05-20 08:02:38 -0700

    data: document 'Cache-Control' header in reference

--- tools/api-client/js/data/header-reference.js
+++ tools/api-client/js/data/header-reference.js
@@ -2,5 +2,6 @@ export const HEADER_REFERENCE = [
   {"name": "Accept", "description": "Media types acceptable for the response."},
   {"name": "Accept-Encoding", "description": "Acceptable content encodings (gzip, br)."},
   {"name": "Accept-Language", "description": "Preferred natural languages."},
-  {"name": "Authorization", "description": "Credentials for authenticating the request."}
+  {"name": "Authorization", "description": "Credentials for authenticating the request."},
+  {"name": "Cache-Control", "description": "Caching directives for requests and responses."}
 ];
