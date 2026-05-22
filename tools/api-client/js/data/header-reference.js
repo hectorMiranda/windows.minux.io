8eacf72e58c1e9f4a3858f352faa6b2c82a694e6
Hector Miranda <marcetux@gmail.com>
2026-05-21 19:19:01 -0700

    data: document 'Content-Type' header in reference

--- tools/api-client/js/data/header-reference.js
+++ tools/api-client/js/data/header-reference.js
@@ -3,5 +3,6 @@ export const HEADER_REFERENCE = [
   {"name": "Accept-Encoding", "description": "Acceptable content encodings (gzip, br)."},
   {"name": "Accept-Language", "description": "Preferred natural languages."},
   {"name": "Authorization", "description": "Credentials for authenticating the request."},
-  {"name": "Cache-Control", "description": "Caching directives for requests and responses."}
+  {"name": "Cache-Control", "description": "Caching directives for requests and responses."},
+  {"name": "Content-Type", "description": "The media type of the body."}
 ];
